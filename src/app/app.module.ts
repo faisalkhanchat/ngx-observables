d6b009c4e2b90e7a3ad6123a6792a6532f09a8f5
unknown <faisalkhan.chat@gmail.com>
2019-01-06 05:34:46 -0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { ServersComponent } from './servers/servers.component';
 import { UsersComponent } from './users/users.component';
 import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { ServerComponent } from './servers/server/server.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app.routing.module';
-import { ServersService } from './servers/servers.service';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
 import { CanDeactivateGaurd } from './page-not-found/can-deactivate-gaurd.service';
 import { ErrorPageComponent } from './error-page/error-page.component';
-import { ServerResolverService } from './servers/server/server-resolver.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ServersComponent,
     UsersComponent,
     UserComponent,
-    EditServerComponent,
-    ServerComponent,
     PageNotFoundComponent,
     ErrorPageComponent
   ],
@@ -37,7 +29,7 @@ import { ServerResolverService } from './servers/server/server-resolver.service'
     FormsModule,
     AppRoutingModule
   ],
-  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGaurd, ServerResolverService],
+  providers: [AuthService, AuthGuard, CanDeactivateGaurd],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
